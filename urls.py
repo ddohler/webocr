4d66f35d145f4804d47a559d29b4594a8a4e70c2
Derek <dohler@gmail.com>
2011-10-31 21:48:29 +0400

    Adding logout.

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-
+from django.contrib.auth.views import login, logout
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
     # Example:
     # (r'^ocrweb/', include('ocrweb.foo.urls')),
     (r'^$', 'interface.views.main.main'),
-    (r'^login$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^login$', login, {'template_name': 'login.html'},name="login"),
+    url(r'^logout$', logout, {'next_page': '/'}, name="logout"),
     # Uncomment the admin/doc line below to enable admin documentation:
     #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
